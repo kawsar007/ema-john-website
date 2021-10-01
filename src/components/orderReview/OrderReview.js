89622fb29968e40d84d75fae0acf5817a30ea53e
unknown <kawsar007>
2021-10-01 11:43:30 +0600

    Implement useCart Hooks & order cart

--- src/components/orderReview/OrderReview.js
+++ src/components/orderReview/OrderReview.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
+import Cart from '../cart/Cart';
 
 function OrderReview() {
-    const [products] = useProducts();
+    const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
     return (
-        <div>
-            <h2>OrderReview Components {products.length}</h2>
+        <div className="shop-container">
+           <div className="product-container">
+            </div>
+            <div className="card-container">
+                <Cart cart={cart}/>
+            </div>
         </div>
     )
 }
